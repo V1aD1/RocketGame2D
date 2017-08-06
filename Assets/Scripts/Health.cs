f4a8835bf1c0da24a4c594e488ea908b38c08031
Vlad Baderca <vladfriends@yahoo.ca>
2017-08-06 15:03:55 +0200

    added health UI

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour {
+
+	public Transform player;
+
+	Text text;
+	float hp;
+
+	// Use this for initialization
+	void Start () {
+		text = GetComponent<Text>();
+		hp = player.GetComponent<Player>().hp;
+		text.color = player.GetComponent<SpriteRenderer>().color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		hp = player.GetComponent<Player>().hp;
+		text.text = hp.ToString();
+
+	}
+}
